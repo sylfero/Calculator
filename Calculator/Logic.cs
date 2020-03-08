c67c17475a5fc4deedb9462ccd352c5beb426cf0
sylfero <43927273+sylfero@users.noreply.github.com>
2020-03-08 21:04:17 +0100

    update

--- Calculator/Logic.cs
+++ Calculator/Logic.cs
@@ -21,9 +21,106 @@ namespace Calculator
                 sender.FontSize = 35;
             }
         }
-        public static void PerformClick(this Button btn)
+        public static void PerformClick(Button btn)
         {
             btn.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+        }
+
+        public static void DoMath(TextBox input, TextBox output, char action, ref bool isFull, ref bool read, ref string last)
+        {
+            string _in = input.Text;
+            string _out = output.Text;
+            if (_in.EndsWith(","))
+            {
+                _in = _in.Replace(",", "");
+                input.Text = _in;
+            }
+            if (action != '=')
+            {
+                if (_out.Length == 0)
+                {
+                    output.Text = _in + ' ' + action;
+                    read = false;
+                }
+                else
+                {
+                    if ((_out.EndsWith("+") || _out.EndsWith("/") || _out.EndsWith("-") || _out.EndsWith("*")) && read == false)
+                    {
+                        output.Text = _out.Remove(_out.Length - 1, 1) + action;
+                    }
+                    else if (_out.EndsWith("="))
+                    {
+                        output.Text = _in + ' ' + action;
+                    }
+                    else
+                    {
+                        output.Text = _out + ' ' + _in  + ' ' + action;
+                        input.Text = Logic.Calculate(_out + ' ' + _in);
+                        read = false;
+                    }
+                }
+            }
+            else
+            {
+                if (_out.Length == 0)
+                {
+                    output.Text = _in + " =";
+                }
+                else
+                {
+                    if (_out.Last() == '=')
+                    {
+                        output.Text = _in + last + " =";
+                        input.Text = Logic.Calculate(_in + last);
+                    }
+                    else
+                    {
+                        last = ' '+ _out.Last().ToString() + ' ' + _in;
+                        input.Text = Logic.Calculate(_out + ' ' + _in);
+                        output.Text = _out + ' ' + _in + " =";
+                    }
+                }
+                read = false;
+            }
+        }
+
+        private static string Calculate(string calc)
+        {
+            List<string> elements = calc.Split(' ').ToList();
+            int index;
+            decimal tmp;
+            while(elements.Count() > 1)
+            {
+                if (elements.Contains("/") || elements.Contains("*"))
+                {
+                    index = elements.FindIndex(x => x == "/" || x == "*");
+                }
+                else
+                {
+                    index = elements.FindIndex(x => x == "+" || x == "-");
+                }
+                
+                if (elements[index] == "/")
+                {
+                    tmp = Decimal.Parse(elements[index - 1]) / Decimal.Parse(elements[index + 1]);
+                }
+                else if (elements[index] == "*")
+                {
+                    tmp = Decimal.Parse(elements[index - 1]) * Decimal.Parse(elements[index + 1]);
+                }
+                else if (elements[index] == "+")
+                {
+                    tmp = Decimal.Parse(elements[index - 1]) + Decimal.Parse(elements[index + 1]);
+                }
+                else
+                {
+                    tmp = Decimal.Parse(elements[index - 1]) - Decimal.Parse(elements[index + 1]);
+                }
+                elements[index] = tmp.ToString();
+                elements.RemoveAt(index + 1);
+                elements.RemoveAt(index - 1);
+            }
+            return elements[0];
         }
     }
 }
--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isFull = false;
+        private bool read = false;
+        private string last;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,15 +33,27 @@ namespace Calculator
         {
             Button button = (Button)sender;
             string num = button.Content.ToString();
-            if (input.Text.Equals("0"))
+            if (read == false)
             {
+                if (memory.Text.Length != 0 && memory.Text.Last() == '=')
+                {
+                    memory.Text = "";
+                }
                 input.Text = num;
+                read = true;
             }
             else
             {
-                if (input.Text.Length < 21)
+                if (input.Text.Equals("0"))
                 {
-                    input.Text += num;
+                    input.Text = num;
+                }
+                else
+                {
+                    if (input.Text.Length < 21)
+                    {
+                        input.Text += num;
+                    }
                 }
             }
             Logic.Resize(input);
@@ -45,31 +61,46 @@ namespace Calculator
 
         private void Action_Click(object sender, RoutedEventArgs e)
         {
-
+            Button btn = (Button)sender;
+            Logic.DoMath(input, memory, btn.Content.ToString().First(), ref isFull, ref read, ref last);
         }
 
         private void Equals_Click(object sender, RoutedEventArgs e)
         {
-
+            Logic.DoMath(input, memory, '=', ref isFull, ref read, ref last);
         }
 
         private void Zero_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!input.Text.Equals("0") && input.Text.Length < 21)
+            {
+                input.Text += '0';
+                Logic.Resize(input);
+                read = true;
+            }
         }
 
         private void Backspace_Click(object sender, RoutedEventArgs e)
         {
-            if (!input.Text.Equals("0"))
+            if (read != false)
             {
-                if ((input.Text.Length == 2 && Double.Parse(input.Text) < 0) || input.Text.Length == 1)
+                if (!input.Text.Equals("0"))
                 {
-                    input.Text = "0";
+                    if ((input.Text.Length == 2 && Decimal.Parse(input.Text) < 0) || input.Text.Length == 1)
+                    {
+                        input.Text = "0";
+                    }
+                    else
+                    {
+                        input.Text = input.Text.Remove(input.Text.Length - 1, 1);
+                    }
                 }
-                else
+            }
+            else
+            {
+                if (memory.Text.Length != 0)
                 {
-
-                    input.Text = input.Text.Remove(input.Text.Length - 1, 1);
+                    memory.Text = "";
                 }
             }
             Logic.Resize(input);
@@ -78,6 +109,7 @@ namespace Calculator
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
             input.Text = "0";
+            read = true;
             Logic.Resize(input);
         }
         
@@ -85,86 +117,94 @@ namespace Calculator
         {
             input.Text = "0";
             memory.Text = "";
+            read = false;
             Logic.Resize(input);
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.Key == Key.D1 || e.Key == Key.NumPad1)
+            
+            if (!(Keyboard.IsKeyDown(Key.RightShift) || Keyboard.IsKeyDown(Key.LeftShift)))
             {
-                Logic.PerformClick(one);
+                if (e.Key == Key.D1 || e.Key == Key.NumPad1)
+                {
+                    Logic.PerformClick(one);
+                }
+                else if (e.Key == Key.D2 || e.Key == Key.NumPad2)
+                {
+                    Logic.PerformClick(two);
+                }
+                else if (e.Key == Key.D3 || e.Key == Key.NumPad3)
+                {
+                    Logic.PerformClick(three);
+                }
+                else if (e.Key == Key.D4 || e.Key == Key.NumPad4)
+                {
+                    Logic.PerformClick(four);
+                }
+                else if (e.Key == Key.D5 || e.Key == Key.NumPad5)
+                {
+                    Logic.PerformClick(five);
+                }
+                else if (e.Key == Key.D6 || e.Key == Key.NumPad6)
+                {
+                    Logic.PerformClick(six);
+                }
+                else if (e.Key == Key.D7 || e.Key == Key.NumPad7)
+                {
+                    Logic.PerformClick(seven);
+                }
+                else if (e.Key == Key.D8 || e.Key == Key.NumPad8)
+                {
+                    Logic.PerformClick(eight);
+                }
+                else if (e.Key == Key.D9 || e.Key == Key.NumPad9)
+                {
+                    Logic.PerformClick(nine);
+                }
+                else if (e.Key == Key.D0 || e.Key == Key.NumPad0)
+                {
+                    Logic.PerformClick(zero);
+                }
+                else if (e.Key == Key.Back)
+                {
+                    Logic.PerformClick(backspace);
+                }
+                else if (e.Key == Key.OemComma || e.Key == Key.OemPeriod)
+                {
+                    Logic.PerformClick(point);
+                }
+                else if (e.Key == Key.OemMinus)
+                {
+                    Logic.PerformClick(minus);
+                }
+                else if (e.Key == Key.Enter || e.Key == Key.OemPlus)
+                {
+                    Logic.PerformClick(equals);
+                }
+                else if (e.Key == Key.OemTilde)
+                {
+                    Logic.PerformClick(divide);
+                }
+                else if (e.Key == Key.Multiply)
+                {
+                    Logic.PerformClick(multiply);
+                }
             }
-            else if (e.Key == Key.D2 || e.Key == Key.NumPad2)
+            else
             {
-                Logic.PerformClick(two);
-            }
-            else if (e.Key == Key.D3 || e.Key == Key.NumPad3)
-            {
-                Logic.PerformClick(three);
-            }
-            else if (e.Key == Key.D4 || e.Key == Key.NumPad4)
-            {
-                Logic.PerformClick(four);
-            }
-            else if (e.Key == Key.D5 || e.Key == Key.NumPad5)
-            {
-                Logic.PerformClick(five);
-            }
-            else if (e.Key == Key.D6 || e.Key == Key.NumPad6)
-            {
-                Logic.PerformClick(six);
-            }
-            else if (e.Key == Key.D7 || e.Key == Key.NumPad7)
-            {
-                Logic.PerformClick(seven);
-            }
-            else if (e.Key == Key.D8 || e.Key == Key.NumPad8)
-            {
-                Logic.PerformClick(eight);
-            }
-            else if (e.Key == Key.D9 || e.Key == Key.NumPad9)
-            {
-                Logic.PerformClick(nine);
-            }
-            else if (e.Key == Key.D0 || e.Key == Key.NumPad0)
-            {
-                Logic.PerformClick(zero);
-            }
-            else if (e.Key == Key.Back)
-            {
-                Logic.PerformClick(backspace);
-            }
-            else if (e.Key == Key.OemPlus)
-            {
-                Logic.PerformClick(add);
-            }
-            else if (e.Key == Key.OemComma || e.Key == Key.OemPeriod)
-            {
-                Logic.PerformClick(point);
-            }
-            else if (e.Key == Key.OemMinus)
-            {
-                Logic.PerformClick(minus);
-            }
-            else if (e.Key == Key.OemFinish)
-            {
-                Logic.PerformClick(equals);
-            }
-            else if (e.Key == Key.OemTilde)
-            {
-                Logic.PerformClick(divide);
-            }
-            else if (e.Key == Key.Multiply)
-            {
-                Logic.PerformClick(multiply);
+                if (e.Key == Key.OemPlus)
+                {
+                    Logic.PerformClick(add);
+                }
             }
         }
 
         private void Negation_Click(object sender, RoutedEventArgs e)
         {
-            if (!input.Text.Equals("0"))
+            if (Decimal.Parse(input.Text) != 0)
             {
-                if (Double.Parse(input.Text) < 0)
+                if (Decimal.Parse(input.Text) < 0)
                 {
                     input.Text = input.Text.Remove(0, 1);
                 }
@@ -176,6 +216,7 @@ namespace Calculator
                     }
                 }
             }
+            read = true;
             Logic.Resize(input);
         }
 
@@ -185,6 +226,15 @@ namespace Calculator
             {
                 input.Text += ',';
                 Logic.Resize(input);
+                read = true;
+            }
+        }
+
+        private void Block_Enter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                e.Handled = true;
             }
         }
     }
